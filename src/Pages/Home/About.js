0b453e415b4f39af0a2ab1ec2bc19f213aafa32b
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-06-11 02:26:59 +0600

    simple typewriter effect added

--- src/Pages/Home/About.js
+++ src/Pages/Home/About.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const About = () => {
     return (
-        <div id='about' className='w-3/5 my-12'>
-            <h1 className='text-5xl font-bold'>Who Am I</h1>
-            <div>
-                <p className='my-4'>
-                    Hi, I'm seam, a junior web developer passionate about developing legendary sites worldwide. At the beginning of my computer career, I used to learn ms word, excel, PowerPoint, etc. To learn that I have to visit different kinds of websites. After gaining some knowledge about that, I was thinking, What if I could make those kinds of websites?
-                </p>
-                <p className='mb-4'>
-                    Immediately, I started searching to learn web development. Then I found a legendary web development course. I enrolled and complete it on time. After that, I realized that I have to learn new things each day to do well in web development. So, I am making learning a regular habit of mine.
-                </p>
-                <p className='mb-4'>
-                    Throughout this time I've learned some technologies like HTML, CSS, JavaScript, React, Bootstrap, Tailwind CSS, and some backend technologies like MongoDB, Express, and Node.js. And I'm passionate to learn more technologies to become a Full-stack web developer.
-                </p>
+        <div>
+            <div id='about' className='w-1/2 mx-auto my-12'>
+                <h1 className='text-6xl font-medium'>
+                    <Typewriter
+                        options={{
+                            autoStart: true,
+                            loop: true,
+                            delay: 175,
+                            deleteSpeed: 150,
+                            strings: ['Who Am I']
+                        }}
+                    />
+                </h1>
+                <hr className='w-3/4 border-green-600' />
+                <div>
+                    <p className='my-4'>
+                        Hi, I'm seam, a junior web developer passionate about developing legendary sites worldwide. At the beginning of my computer career, I used to learn ms word, excel, PowerPoint, etc. To learn that I have to visit different kinds of websites. After gaining some knowledge about that, I was thinking, What if I could make those kinds of websites?
+                    </p>
+                    <p className='mb-4'>
+                        Immediately, I started searching to learn web development. Then I found a legendary web development course. I enrolled and complete it on time. After that, I realized that I have to learn new things each day to do well in web development. So, I am making learning a regular habit of mine.
+                    </p>
+                    <p className='mb-4'>
+                        Throughout this time I've learned some technologies like HTML, CSS, JavaScript, React, Bootstrap, Tailwind CSS, and some backend technologies like MongoDB, Express, and Node.js. And I'm passionate to learn more technologies to become a Full-stack web developer.
+                    </p>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import manWithLaptop from '../../assets/images/man-with-laptop.svg';
 import resume from '../../assets/Mobashirul-Alam-Seam-Resume.pdf';
+import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
     return (
@@ -10,7 +11,17 @@ const Banner = () => {
                 <div>
                     <h3 className='mb-2 ml-1'>Hi there, I'm</h3>
                     <h1 class="text-5xl md:text-7xl font-bold">MOBASHIRUL <br /> ALAM SEAM</h1>
-                    <h2 className='text-xl md:text-2xl'>JUNIOR WEB DEVELOPER</h2>
+                    <h2 className='text-xl md:text-2xl underline'>
+                        <Typewriter
+                            options={{
+                                autoStart: true,
+                                loop: true,
+                                delay: 'natural',
+                                strings: ['JUNIOR WEB DEVELOPER']
+                            }}
+                        />
+
+                    </h2>
                     <p class="py-2 md:py-4">
                         I'm a Junior Web Developer with front-end along with <br /> back-end development skills.
                     </p>
